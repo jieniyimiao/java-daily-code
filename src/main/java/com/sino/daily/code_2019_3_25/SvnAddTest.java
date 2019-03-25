33a41e13af744479021f2d465eb87fd62c6f266d
jieniyimiao <940433899@qq.com>
2019-03-25 20:38:48 +0800

    [Bug/Story ID]： [Bug/Story概述]： [修改描述]： 第一个文件 [修改人]： [审核人]：

---
+++ src/main/java/com/sino/daily/code_2019_3_25/SvnAddTest.java
@@ -0,0 +1,13 @@
+package com.sino.daily.code_2019_3_25;
+
+/**
+ * Created on 2019/3/25 20:35.
+ *
+ * @author caogu
+ */
+public class SvnAddTest {
+    public static void main(String[] args) {
+        System.out.println("git 不能添加和提交一个空目录");
+    }
+
+}
