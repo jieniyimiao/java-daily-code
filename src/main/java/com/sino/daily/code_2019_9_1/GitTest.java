91588fb6f22b44cf0c7344f95dc463900d4ab3e4
jieniyimiao <940433899@qq.com>
2019-09-01 10:44:42 +0800

    first commit!

---
+++ src/main/java/com/sino/daily/code_2019_9_1/GitTest.java
@@ -0,0 +1,12 @@
+package com.sino.daily.code_2019_9_1;
+
+/**
+ * create by 2019-09-01 09:51
+ *
+ * @author caogu
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("git test project!");
+    }
+}
